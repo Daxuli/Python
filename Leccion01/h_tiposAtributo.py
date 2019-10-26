3c31bf7cba954d3827f0d8a8c139afaaeefd2fe6
Daxuli <daxulixuli@gmail.com>
2019-10-26 15:01:16 +0200

    Update h_tiposAtributo.py

--- Leccion01/h_tiposAtributo.py
+++ Leccion01/h_tiposAtributo.py
@@ -1,17 +1,42 @@
-__author__ = 'graficos'
+"""
+Attribute types
+see also:
+https://www.tutorialsteacher.com/python/private-and-protected-access-modifiers-in-python
+"""
+import time
 
 
+class Safe:
+    def __init__(self, secret, pin):  # secured
+        self.secret = secret  # public
+        self._secret = self.secret  # protected
+        self.__PIN = pin  # private
 
-class CajaFuerte():
-    def __init__(self,secreto, PIN):#protegidas
-        self.secreto = secreto#visibles
-        self._secreto = self.secreto#privadas
-        self.__PIN = PIN#ocultas
+    def get_password(self):
+        return self.__PIN
 
-micajafuerte = CajaFuerte("le gusta mas FORTRAN", "0000")
+    def change_password(self, pin):
+        self.__PIN = pin
 
-print(micajafuerte._secreto)
-micajafuerte.secreto = "ademas en Notepard"
-print(micajafuerte._secreto)
 
-print(micajafuerte._CajaFuerte__PIN)
+mySafe = Safe("he prefers FORTRAN", "0000")
+
+# Accessing/Modifying public variables: OK
+print(mySafe.secret)
+mySafe.secret = "he prefers MATLAB"
+print(mySafe.secret)
+print("-----")
+# Accessing/Modifying protected variables: can be done but not a good practice
+print(mySafe._secret)
+mySafe._secret = "using NOTEPAD"
+print(mySafe._secret)
+print("-----")
+# Accessing/Modifying private variables: can't be done outside the class
+print(mySafe.get_password())
+mySafe.change_password("1234")
+print(mySafe.get_password())
+mySafe._Safe__PIN = ("9999")  # could be modified/accessed this way, but the variable is private for a reason
+print(mySafe._Safe__PIN)
+
+time.sleep(1)
+print(mySafe.__PIN)  # trying to access it directly will trigger an error
