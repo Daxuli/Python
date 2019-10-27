eda8e0724d0ceaae6721c809acb47f42866ed901
Daxuli <daxulixuli@gmail.com>
2019-10-27 14:09:21 +0100

    Update d_DeclaracionCirculo.py

--- Leccion02/d_DeclaracionCirculo.py
+++ Leccion02/d_DeclaracionCirculo.py
@@ -1,20 +1,25 @@
+"""
+Some additional resources for the new concepts:
+https://realpython.com/python-super/
+https://www.programiz.com/python-programming/property
+"""
 import math
 import Leccion02.c_DeclaracionPunto
 
 
 class Circulo(Leccion02.c_DeclaracionPunto.Punto):
     def __init__(self, x, y, radio):
-        super().__init__(x, y)
-        self.radio = radio
+        super(Circulo, self).__init__(x, y)
+        self.radio = radio  # calls to the @radius.setter and saves the variable at _radius
 
-    @property
-    def area(self):
-        self._area = math.pi*self.radio**2
-        return self._area
+    @property  # the first time is for the getter
+    def area(self):  # we want to know the area of the circle, which was defined with its radius
+        self._area = math.pi*self.radio**2  # calls to the @radius.getter and takes the value from _radius
+        return self._area  # private variable
 
-    @area.setter
-    def area(self, area):
-        self.radio = math.sqrt(area/math.pi)
+    @area.setter  # defining its setter function
+    def area(self, area):  # we are setting the area, so the radius must change
+        self.radio = math.sqrt(area/math.pi)  # calls to the @radius.setter and saves the variable at _radius
 
     @property
     def radio(self):
@@ -39,3 +44,9 @@ print(c1.area, c1.radio)
 
 c1.area = 19
 print(c1.area, c1.radio)
+
+"""
+Let's see which attributes c1 has
+"""
+print("-----")
+print(c1.__dict__)  # _radius and _area are its actual attributes
